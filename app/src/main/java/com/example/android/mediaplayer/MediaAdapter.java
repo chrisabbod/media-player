98acb1df9ab30d222c21fd85e4ae0cc388aeccb7
Chris <chrisabbod@gmail.com>
2018-05-26 21:26:00 -0400

    <feat> Create list_item Layout
    Create list_item xml layout to display individual views on activity_main layout

--- app/src/main/java/com/example/android/mediaplayer/MediaAdapter.java
+++ app/src/main/java/com/example/android/mediaplayer/MediaAdapter.java
@@ -1,8 +1,33 @@
 package com.example.android.mediaplayer;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class MediaAdapter{
+import java.util.ArrayList;
 
+public class MediaAdapter extends ArrayAdapter<Media>{
+
+    public MediaAdapter(Activity context, ArrayList<Media> media){
+        super(context, 0, media);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Media currentMedia = getItem(position);
+
+        TextView mediaTextView = (TextView)listItemView.findViewById(R.id.media_text_view);
+        mediaTextView.setText(currentMedia.getMediaTitle());
+
+        return listItemView;
+    }
 }
